cb10e190f9d9698a1a5ab001d5409a80a6670040
tt <tt@tts-Mac-mini.local>
2025-04-17 15:58:22 +0100

    fix for animations

--- whoami/script.js
+++ whoami/script.js
@@ -1,11 +1,8 @@
 window.onload = (event) => {
   console.log("loaded");
   document.getElementById("titlebar").innerHTML = "...";
-  animatetitle(
-    document.getElementById("textside").innerHTML,
-    0.5,
-    false,
-    "textside",
-  );
+  let b = document.getElementById("textside").innerHTML;
+  document.getElementById("textside").innerHTML = "";
+  animatetitle(b, 0.5, false, "textside");
   checkmobile();
 };
